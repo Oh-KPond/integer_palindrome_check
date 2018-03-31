af25d012221e4464f357f2f6addda0bc0ba25511
Kate Pond <kate@thepondsedge.com>
2018-03-31 08:36:25 -0700

    completes palindrome check code

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,20 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
+require "pry"
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0 || number == ""
+
+  compair_number = 0
+  num = number
+
+  while  num > 0
+    temp = num % 10
+    compair_number = compair_number * 10 + temp
+    num /= 10
+  end
+
+  if compair_number == number
+    return true
+  else
+    return false
+  end
 end
